d2abf2d689a290bf5c8ca8d59d37c7fc2403eb42
surendranathj <surendranathj>
2024-09-07 05:35:34 +0000

    added code01

--- notebookintegration.py
+++ notebookintegration.py
@@ -1,2 +1,12 @@
 # Databricks notebook source
 df_01=spark.read.csv('/Volumes/workdatanbricks78/default/vol8/cars_seaborn_dataset.csv')
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC show tables
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC show catalogs
