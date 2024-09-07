dc26884242d849de785ad128b2743f1f2dbf270f
surendranathj <surendranathj>
2024-09-07 05:34:16 +0000

    adding df_01

---
+++ Untitled Notebook 2024-09-07 11_00_46.py
@@ -0,0 +1,2 @@
+# Databricks notebook source
+df_01=spark.read.
---
+++ notebookintegration.py
@@ -0,0 +1,2 @@
+# Databricks notebook source
+df_01=spark.read.csv('/Volumes/workdatanbricks78/default/vol8/cars_seaborn_dataset.csv')
